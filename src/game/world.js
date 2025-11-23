7662dc4ae235807e01449ee272b00fe9a64773ff
makej7403-bit <makej7403@gmail.com>
2025-11-23 23:51:48 +0000

    Update world.js

--- src/game/world.js
+++ src/game/world.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { City } from "./city.js";
 
 export class World {
   constructor(scene) {
@@ -6,20 +7,21 @@ export class World {
 
     this.addLights();
     this.addGround();
+    this.city = new City(scene);
   }
 
   addLights() {
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(5, 10, 5);
-    this.scene.add(light);
+    const sun = new THREE.DirectionalLight(0xffffff, 1);
+    sun.position.set(5, 20, 5);
+    this.scene.add(sun);
 
     const ambient = new THREE.AmbientLight(0xffffff, 0.4);
     this.scene.add(ambient);
   }
 
   addGround() {
-    const groundGeo = new THREE.PlaneGeometry(500, 500);
-    const groundMat = new THREE.MeshPhongMaterial({ color: 0x808080 });
+    const groundGeo = new THREE.PlaneGeometry(2000, 2000);
+    const groundMat = new THREE.MeshPhongMaterial({ color: 0x4d4d4d });
     const ground = new THREE.Mesh(groundGeo, groundMat);
 
     ground.rotation.x = -Math.PI / 2;
