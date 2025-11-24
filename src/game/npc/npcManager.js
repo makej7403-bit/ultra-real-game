650914ea2d26ad73ce0926a342b9d2f7dbc84774
makej7403-bit <makej7403@gmail.com>
2025-11-24 00:18:10 +0000

    Update npcManager.js

--- src/game/npc/npcManager.js
+++ src/game/npc/npcManager.js
@@ -1,3 +1,4 @@
+// src/game/npc/npcManager.js
 import * as THREE from "three";
 import { NPC } from "./npc.js";
 
@@ -5,18 +6,12 @@ export class NPCManager {
   constructor(scene) {
     this.scene = scene;
     this.npcs = [];
-
     this.spawnNPCs();
   }
 
   spawnNPCs() {
-    for (let i = 0; i < 25; i++) {
-      const pos = new THREE.Vector3(
-        (Math.random() - 0.5) * 200,
-        1,
-        (Math.random() - 0.5) * 200
-      );
-
+    for (let i = 0; i < 28; i++) {
+      const pos = new THREE.Vector3((Math.random()-0.5)*600, 1, (Math.random()-0.5)*600);
       const npc = new NPC(this.scene, pos);
       this.scene.add(npc.npc);
       this.npcs.push(npc);
@@ -24,6 +19,6 @@ export class NPCManager {
   }
 
   update(playerPos, carPos) {
-    this.npcs.forEach((npc) => npc.update(playerPos, carPos));
+    this.npcs.forEach(npc => npc.update(playerPos, carPos));
   }
 }
